f4970b9f94241792dacc990b787d6928a0837d0f
MatthewEnamuotor <90224141+oviematthew@users.noreply.github.com>
2023-09-30 09:50:32 -0400

    Initial Commit

---
+++ hangmanApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hangmanApp
+//
+//  Created by Matthew Ovie Enamuotor on 30/09/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
