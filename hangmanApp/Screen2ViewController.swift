be1a8f47a4d79c5e888c03db1cf74e012eec9095
MatthewEnamuotor <90224141+oviematthew@users.noreply.github.com>
2023-10-27 07:38:35 -0400

    updated vc code

--- hangmanApp/Screen2ViewController.swift
+++ hangmanApp/Screen2ViewController.swift
@@ -8,6 +8,49 @@
 import UIKit
 
 class Screen2ViewController: UIViewController {
+    
+    
+    let wordsArray = [
+        "pizzazz", "zyzzyva", "fuzzbox", "pizazzy", "jacuzzi",
+        "jazzman", "jazzmen", "jazzbos", "jazzily", "jazzing",
+        "zizzing", "quizzed", "zizzled", "quizzer", "quizzes",
+        "wazzock", "zizzles", "buzzwig", "jazzers", "jazzier",
+        "buzzcut", "muzjiks", "buzzing", "buzzsaw", "fuzzily",
+        "fuzzing", "muzzily", "schizzy", "abbozzo", "chazzan",
+        "chazzen", "fizzily", "fizzing", "frizzly", "jejunum",
+        "jimjams", "jukebox", "jumbuck", "mezuzah", "muzzled",
+        "puzzled", "schnozz", "wazzing", "whizzed", "zhuzhed",
+        "buzzard", "fizzled", "grizzly", "guzzled", "huzzahs",
+        "jujubes", "jujuism", "mizzled", "muezzin", "muzzler",
+        "muzzles", "puzzler", "puzzles", "whizzer", "whizzes",
+        "zhuzhes", "bezique", "bezzant", "buzzers", "buzzier",
+        "cazique", "dizzily", "drizzly", "fizzles", "frazzle",
+        "frizzed", "frizzle", "fuzzier", "guzzler", "guzzles",
+        "huzzaed", "jezebel", "jumpoff", "karezza", "mazzard",
+        "mezquit", "mezuzas", "mezuzot", "mizzens", "mizzles",
+        "muzzier", "nuzzled", "palazzi", "palazzo", "pizzles",
+        "sizzurp", "skyjack", "swizzle", "twizzle", "wizzens",
+        "zigzags", "bezzies", "bizzies", "cozzies", "dazzled",
+        "fizzers", "fizzier", "frizzer", "frizzes", "gizzard",
+        "grizzle", "hazzans", "jejunal", "jojobas", "jonquil",
+        "jujutsu", "jujuist", "jumpcut", "kibbutz", "kolkhoz",
+        "kolkozy", "lockjaw", "nozzles", "packwax", "quetzal",
+        "quezals", "quickly", "sizzled", "sozzled", "ziplock",
+        "zombify", "blowzed", "buxomly", "cachexy", "cajuput",
+        "chametz", "chintzy", "chometz", "complex", "cozying",
+        "cumquat", "fowlpox", "hutzpah", "hypoxic", "jackals",
+        "jackeen", "jauking", "jawfish", "jawlike", "jewfish",
+        "jibbahs", "jinking", "jobbery", "jouking", "joypops",
+        "jubhahs", "jugband", "jugfuls", "jugging", "jugsful",
+        "jumbals", "jumbler", "jumbles", "kajeput", "kebbock",
+        "kickoff", "killjoy", "klezmer", "kwanzas", "machzor",
+        "mahjong", "maximum", "maxixes", "mazurka", "moujiks",
+        "muntjac", "mzungus", "outjump", "oxazine", "oxazole",
+        "oxidize", "pickaxe", "punjabi", "pyjamas", "quaffed",
+        "quakily", "quaking", "quibble", "quicker", "quickie",
+        "quipped", "quippus", "quixote", "showbiz", "squawks",
+        "tickbox", "upchuck", "steezed", "squeaky", "squozen"
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -15,16 +58,6 @@ class Screen2ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
    
     @IBAction func onExitBtnTapped(_ sender: UIButton) {
--- hangmanApp/ViewController.swift
+++ hangmanApp/ViewController.swift
@@ -10,47 +10,7 @@ import UIKit
 class ViewController: UIViewController {
 
     
-    let wordsArray = [
-        "pizzazz", "zyzzyva", "fuzzbox", "pizazzy", "jacuzzi",
-        "jazzman", "jazzmen", "jazzbos", "jazzily", "jazzing",
-        "zizzing", "quizzed", "zizzled", "quizzer", "quizzes",
-        "wazzock", "zizzles", "buzzwig", "jazzers", "jazzier",
-        "buzzcut", "muzjiks", "buzzing", "buzzsaw", "fuzzily",
-        "fuzzing", "muzzily", "schizzy", "abbozzo", "chazzan",
-        "chazzen", "fizzily", "fizzing", "frizzly", "jejunum",
-        "jimjams", "jukebox", "jumbuck", "mezuzah", "muzzled",
-        "puzzled", "schnozz", "wazzing", "whizzed", "zhuzhed",
-        "buzzard", "fizzled", "grizzly", "guzzled", "huzzahs",
-        "jujubes", "jujuism", "mizzled", "muezzin", "muzzler",
-        "muzzles", "puzzler", "puzzles", "whizzer", "whizzes",
-        "zhuzhes", "bezique", "bezzant", "buzzers", "buzzier",
-        "cazique", "dizzily", "drizzly", "fizzles", "frazzle",
-        "frizzed", "frizzle", "fuzzier", "guzzler", "guzzles",
-        "huzzaed", "jezebel", "jumpoff", "karezza", "mazzard",
-        "mezquit", "mezuzas", "mezuzot", "mizzens", "mizzles",
-        "muzzier", "nuzzled", "palazzi", "palazzo", "pizzles",
-        "sizzurp", "skyjack", "swizzle", "twizzle", "wizzens",
-        "zigzags", "bezzies", "bizzies", "cozzies", "dazzled",
-        "fizzers", "fizzier", "frizzer", "frizzes", "gizzard",
-        "grizzle", "hazzans", "jejunal", "jojobas", "jonquil",
-        "jujutsu", "jujuist", "jumpcut", "kibbutz", "kolkhoz",
-        "kolkozy", "lockjaw", "nozzles", "packwax", "quetzal",
-        "quezals", "quickly", "sizzled", "sozzled", "ziplock",
-        "zombify", "blowzed", "buxomly", "cachexy", "cajuput",
-        "chametz", "chintzy", "chometz", "complex", "cozying",
-        "cumquat", "fowlpox", "hutzpah", "hypoxic", "jackals",
-        "jackeen", "jauking", "jawfish", "jawlike", "jewfish",
-        "jibbahs", "jinking", "jobbery", "jouking", "joypops",
-        "jubhahs", "jugband", "jugfuls", "jugging", "jugsful",
-        "jumbals", "jumbler", "jumbles", "kajeput", "kebbock",
-        "kickoff", "killjoy", "klezmer", "kwanzas", "machzor",
-        "mahjong", "maximum", "maxixes", "mazurka", "moujiks",
-        "muntjac", "mzungus", "outjump", "oxazine", "oxazole",
-        "oxidize", "pickaxe", "punjabi", "pyjamas", "quaffed",
-        "quakily", "quaking", "quibble", "quicker", "quickie",
-        "quipped", "quippus", "quixote", "showbiz", "squawks",
-        "tickbox", "upchuck", "steezed", "squeaky", "squozen"
-    ]
+   
 
     
     override func viewDidLoad() {
